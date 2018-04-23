3327e31ebcccf199e06634d3c6c0d97e9bbae18f
Justin Kimbrell <justin@objectivehtml.com>
2018-04-23 09:56:29 -0600

    Fixed issue with http baseurl settings hardcoded

--- src/Config/Http.js
+++ src/Config/Http.js
@@ -1,5 +1,20 @@
+let domain;
+
+switch(window.location.hostname) {
+    case 'localhost':
+    case 'giveworks.test':
+        domain = 'https://giveworks.test';
+        break;
+    case 'dev5.giveworks.dev':
+        domain = 'https://dev5.giveworks.net';
+        break;
+
+    default:
+        domain = 'https://secure.giveworks.net';
+}
+
 export default {
 
-    baseURL: 'https://giveworks.test/api/public/v1/'
+    baseURL: `${domain}/api/public/v1/`
 
 }

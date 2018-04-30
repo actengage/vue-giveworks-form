f99684b813bb7ed5e22153b925e2c1a5e6c40b38
Justin Kimbrell <justin@objectivehtml.com>
2018-04-30 07:56:00 -0600

    0.1.115

--- src/Config/Http.js
+++ src/Config/Http.js
@@ -1,14 +1,15 @@
 let domain;
 
+console.log('http config', domain);
+
 switch(window.location.hostname) {
     case 'localhost':
     case 'giveworks.test':
         domain = 'https://giveworks.test';
         break;
-    case 'dev5.giveworks.dev':
+    case 'dev5.giveworks.net':
         domain = 'https://dev5.giveworks.net';
         break;
-
     default:
         domain = 'https://secure.giveworks.net';
 }
